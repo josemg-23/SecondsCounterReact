100a9b10bd0d5cf038aef2542c5feb212d147005
josemg-23 <jose.gomezt19@gmail.com>
2022-04-22 23:36:45 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,18 +4,16 @@ import React, { useState } from "react";
 
 //create your first component
 
-const Counter = () => {
+const Counter = (props) => {
 	let [seconds, setSeconds] = useState(0);
 	setInterval(function () {
 		setSeconds(seconds + 1);
 	}, 1000);
 	return (
 		<div className="bigcounter">
-			<div className="number digit2">
-				{Math.floor(seconds / 100) % 10}
-			</div>
-			<div className="number digit3">{Math.floor(seconds / 10) % 10}</div>
-			<div className="number digit4">{Math.floor(seconds / 1) % 10}</div>
+			<div className="number">{Math.floor(seconds / 100) % 10}</div>
+			<div className="number">{Math.floor(seconds / 10) % 10}</div>
+			<div className="number">{Math.floor(seconds / 1) % 10}</div>
 		</div>
 	);
 };
